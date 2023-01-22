30b90c039602710c2d8d6603ace2eda90eca684a
yulog <lepton115@gmail.com>
2023-01-22 20:50:05 +0900

    add files

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
 # go-gomamayo
+
+[ゴママヨ](https://thinaticsystem.com/glossary/gomamayo)検出器のGo言語版です。[na2na-p/gomamayo-deno](https://github.com/na2na-p/gomamayo-deno)を参考にした部分的な再実装です。
+形態素解析の辞書にはIPADICを使っています。
+
+## Library
+
+```
+go get -u github.com/yulog/go-gomamayo@latest
+```
+
+## CLI
+```
+go install github.com/yulog/go-gomamayo/cmd/gomamayo@latest
+```
+
+### Example
+
+```
+gomamayo analyze ごまマヨネーズ
+```
+
+```json
+{
+  "isGomamayo": true,
+  "combo": 1,
+  "detail": [
+    {
+      "surface": "ごま|マヨネーズ",
+      "dimension": 1,
+      "rawResult1": {
+        "id": 28368,
+        "start": 0,
+        "end": 2,
+        "surface": "ごま",
+        "class": "KNOWN",
+        "pos": [
+          "名詞",
+          "一般",
+          "*",
+          "*"
+        ],
+        "base_form": "ごま",
+        "reading": "ゴマ",
+        "pronunciation": "ゴマ",
+        "features": [
+          "名詞",
+          "一般",
+          "*",
+          "*",
+          "*",
+          "*",
+          "ごま",
+          "ゴマ",
+          "ゴマ"
+        ]
+      },
+      "rawResult2": {
+        "id": 99158,
+        "start": 2,
+        "end": 7,
+        "surface": "マヨネーズ",
+        "class": "KNOWN",
+        "pos": [
+          "名詞",
+          "一般",
+          "*",
+          "*"
+        ],
+        "base_form": "マヨネーズ",
+        "reading": "マヨネーズ",
+        "pronunciation": "マヨネーズ",
+        "features": [
+          "名詞",
+          "一般",
+          "*",
+          "*",
+          "*",
+          "*",
+          "マヨネーズ",
+          "マ ヨネーズ",
+          "マヨネーズ"
+        ]
+      }
+    }
+  ]
+}
+```
+
+
+## License
+
+MIT
+
+## Author
+
+yulog
---
+++ gomamayo.go
@@ -0,0 +1,106 @@
+package gomamayo
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/goark/krconv"
+	"github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome/v2/tokenizer"
+)
+
+var vowel = map[string]string{"a": "ア", "i": "イ", "u": "ウ", "e": "エ", "o": "オ"}
+
+type GomamayoResult struct {
+	IsGomamayo bool             `json:"isGomamayo"`
+	Combo      int              `json:"combo"`
+	Detail     []GomamayoDetail `json:"detail"`
+}
+
+type GomamayoDetail struct {
+	Surface    string              `json:"surface"`
+	Dimension  int                 `json:"dimension"`
+	RawResult1 tokenizer.TokenData `json:"rawResult1"`
+	RawResult2 tokenizer.TokenData `json:"rawResult2"`
+}
+
+// kagomeで解析する
+func tokenize(input string) []tokenizer.Token {
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		panic(err)
+	}
+	return t.Tokenize(input)
+}
+
+// 長音を変換する
+func prolongedSoundMarkVowelize(reading string) (returnReading string) {
+	readingRune := []rune(reading)
+	prev := ""
+	for i := 0; i < len(readingRune); i++ {
+		current := string(readingRune[i])
+		if current == "ー" {
+			roman := []rune(krconv.Convert(prev))
+			returnReading += vowel[string(roman[len(roman)-1])]
+		} else {
+			returnReading += current
+		}
+		prev = current
+	}
+	return returnReading
+}
+
+// Analyze は input がゴママヨか判定する
+func Analyze(input string) (gomamayoResult GomamayoResult) {
+	tokens := tokenize(input)
+
+	// for _, token := range tokens {
+	// 	features := strings.Join(token.Features(), ",")
+	// 	fmt.Printf("%s\t%v\n", token.Surface, features)
+	// }
+
+	vowelizedReading := []string{}
+
+	for _, token := range tokens {
+		if reading, ok := token.Reading(); ok {
+			if strings.Contains(reading, "ー") {
+				vowelizedReading = append(vowelizedReading, prolongedSoundMarkVowelize(reading))
+			} else {
+				vowelizedReading = append(vowelizedReading, reading)
+			}
+		}
+	}
+
+	for i := 0; i < len(tokens)-1; i++ {
+		first := tokens[i]
+		second := tokens[i+1]
+
+		if first.POS()[0] != "名詞" && first.POS()[0] != "数詞" || second.Surface == first.Surface {
+			continue
+		}
+
+		_, ok1 := first.Reading()
+		_, ok2 := second.Reading()
+		reading1 := vowelizedReading[i]
+		reading2 := vowelizedReading[i+1]
+		if ok1 && ok2 {
+			minLen := min(len([]rune(reading1)), len([]rune(reading2)))
+			for j := 1; j < minLen; j++ {
+				fragment1 := string([]rune(reading1)[len([]rune(reading1))-j:])
+				fragment2 := string([]rune(reading2)[0:j])
+				if fragment1 == fragment2 {
+					gomamayoResult.IsGomamayo = true
+					gomamayoResult.Detail = append(gomamayoResult.Detail, GomamayoDetail{
+						Surface:    fmt.Sprintf("%s|%s", first.Surface, second.Surface),
+						Dimension:  j,
+						RawResult1: tokenizer.NewTokenData(first),
+						RawResult2: tokenizer.NewTokenData(second),
+					})
+					gomamayoResult.Combo++
+				}
+			}
+		}
+	}
+
+	return gomamayoResult
+}
