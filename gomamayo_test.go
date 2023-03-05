5f7ba3b2fede98357f1311478dd530e974e7258f
yulog <29723448+yulog@users.noreply.github.com>
2023-03-05 20:13:17 +0900

    add fuzzing

---
+++ gomamayo_test.go
@@ -0,0 +1,15 @@
+package gomamayo
+
+import (
+	"testing"
+)
+
+func FuzzAnalyze(f *testing.F) {
+	testcases := []string{"ごまマヨネーズ", " ", "メモリリーク", "メモリ、リーク。", "独自実装", "ダイレクト投稿", "部分分数分解"}
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, s string) {
+		Analyze(s)
+	})
+}
